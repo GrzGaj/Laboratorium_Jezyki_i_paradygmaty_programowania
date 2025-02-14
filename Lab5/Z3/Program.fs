61636cb0a14404409303644c1eacd3fa7cdcee93
GrzGaj <128969118+GrzGaj@users.noreply.github.com>
2025-02-14 20:59:46 +0100

    Add files via upload

---
+++ Lab5/Z3/Program.fs
@@ -0,0 +1,14 @@
+﻿//Zadanie 3 
+
+let nums = [1; 2; 3]
+let rec permutations lst =
+    match lst with
+    | [] -> [[]]  
+    | x :: xs -> 
+        let perms = permutations xs
+        [ for perm in perms do
+            for i in 0 .. List.length perm do
+                yield List.insertAt i x perm ]
+
+let result = permutations nums
+printfn "%A" result
