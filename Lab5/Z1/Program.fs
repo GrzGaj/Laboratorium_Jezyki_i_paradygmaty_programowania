b26fd6d6c6a4df3f54dde0002655fb2d8f7b4d35
GrzGaj <128969118+GrzGaj@users.noreply.github.com>
2025-02-14 20:58:33 +0100

    Add files via upload

---
+++ Lab5/Z1/Program.fs
@@ -0,0 +1,21 @@
+﻿// Zadanie 1
+
+//wersja rekurencyjna
+let rec Fib n =
+    match n with
+    |0 -> 0
+    |1 -> 1
+    |_ -> Fib(n-1) + Fib (n-2)
+
+//wersja rekurencyjna ogonowa
+let FibTail n =
+    let rec aux n a b =
+        if n<= 0 then a
+        elif n= 1 then b
+        else aux (n-1) b (a+b)
+    aux n 0 1
+
+let fibResult = Fib 10
+printfn "Wyniki: %d" fibResult
+let fibTailResult = FibTail 10
+printfn "Wyniki: %d" fibTailResult
---
+++ Lab5/Z2/Program.fs
@@ -0,0 +1,42 @@
+﻿// Zadanie 2 
+
+//definicja wykorzystywanego typu
+type BinarTree<'T>=
+    | Empty
+    | Node of 'T * BinarTree<'T> * BinarTree<'T>
+//struktura drzewa
+let tree = Node(10, 
+            Node(5,
+                Node(1, Empty, Empty),
+                Node(18,Empty,Empty)
+            ),
+            Node(5,
+                Node(51, Empty, Empty),
+                Node(3,Empty,Empty)
+            )
+            )
+//wyszukiwanie rekurencyjne
+let rec szukajRec tree value =
+    match tree with
+    | Empty -> false
+    | Node (v, left, right) ->
+        if v = value then true
+        else szukajRec left value || szukajRec right value
+//wyszukiwanie iteracyjne
+let szukajItr tree value =
+    let rec iter stack =
+        match stack with
+        | [] -> false
+        | Empty :: rest -> iter rest
+        | Node (v, left, right) :: rest ->
+            if v = value then true
+            else iter (right :: left :: rest)
+    
+    iter [tree]
+
+
+let wynikRec = szukajRec tree 21   
+let wynikItr = szukajItr tree 5
+
+printfn "Wynik wyszukiwania rekurencyjnego %b" wynikRec
+printfn "Wynik wyszukiwania iteracyjnego %b" wynikItr
